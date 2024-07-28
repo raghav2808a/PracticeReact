4622b7f7a42b23980b233bbb759e36c0da9127c7
raghav2808a <raghavagravat101@gmail.com>
2024-07-28 15:41:56 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+
+
+// <div id="parent">
+//     <div id="parent">
+//         <h1>raghav 1</h1>
+//         <h2>raghav 2</h2>
+//     </div>
+// </div>
+
+const hedding=React.createElement("div",{id:'parent'},
+  [
+    React.createElement("div",{id:'Child'},[
+        React.createElement("h1",{id:'raghav'},"Raghav Agravat :) 1"),
+        React.createElement("h2",{id:'raghav2'},"Raghav Agravat :) 2"),
+
+    ]),
+    React.createElement("div",{id:'Child2'},[
+        React.createElement("h1",{id:'raghav3'},"Raghav Agravat :) 4"),
+        React.createElement("h2",{id:'raghav4'},"Raghav Agravat :) 5"),
+
+    ])
+
+  ]
+);
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(hedding);
